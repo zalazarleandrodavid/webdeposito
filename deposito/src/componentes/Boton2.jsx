bcdab1fe824859364853ae9f4d7b1f5e620cab48
zalazarleandrodavid <zalazarleandrodavid05@gmail.com>
2023-05-28 17:32:20 -0300

    codigo

--- deposito/src/componentes/Boton2.jsx
+++ deposito/src/componentes/Boton2.jsx
@@ -1,43 +1,54 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import Quagga from 'quagga';
 
 const Boton2 = ({ onBarcodeScan }) => {
   const scannerRef = useRef(null);
+  const [scannedBarcode, setScannedBarcode] = useState('');
 
   const openBarcodeScanner = () => {
-    Quagga.init({
-      inputStream: {
-        name: "Live",
-        type: "LiveStream",
-        target: scannerRef.current,
-        constraints: {
-          facingMode: "environment"
+    Quagga.init(
+      {
+        inputStream: {
+          name: "Live",
+          type: "LiveStream",
+          target: scannerRef.current,
+          constraints: {
+            facingMode: "environment"
+          },
         },
+        decoder: {
+          readers: ['ean_reader']
+        }
       },
-      decoder: {
-        readers: ['ean_reader']
+      (err) => {
+        if (err) {
+          console.error('Error al inicializar Quagga:', err);
+          return;
+        }
+
+        Quagga.start();
+
+        Quagga.onDetected((result) => {
+          const barcode = result.codeResult.code;
+          Quagga.stop();
+
+          setScannedBarcode(barcode); // Actualiza el estado con el código de barras escaneado
+          if (typeof onBarcodeScan === 'function') {
+            onBarcodeScan(barcode);
+          }
+
+          // Abre una nueva ventana del navegador con la información del código de barras
+          
+        });
       }
-    }, (err) => {
-      if (err) {
-        console.error('Error al inicializar Quagga:', err);
-        return;
-      }
-
-      Quagga.start();
-
-      Quagga.onDetected((result) => {
-        const barcode = result.codeResult.code;
-        Quagga.stop();
-
-        onBarcodeScan(barcode);
-      });
-    });
+    );
   };
 
   return (
     <div>
       <button onClick={openBarcodeScanner}>Telefonica</button>
       <div ref={scannerRef} style={{ width: '100%' }}></div>
+      {scannedBarcode && <p>Código de barras escaneado: {scannedBarcode}</p>}
     </div>
   );
 };
